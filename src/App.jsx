8d7d892f0d25f06ee961352a939c175a828db9f5
Theo <theo.piedade@gmail.com>
2023-07-16 09:57:07 -0300

    Logo OK

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,10 @@ function App() {
   return (
    <Body>
    <ContainerApp>
-	Teste
+	<Logo>
+		<img src="/assets/logo.png"/>
+		<h1>ZapRecall</h1>
+	</Logo>
    </ContainerApp>
    </Body>
   )
@@ -38,3 +41,25 @@ const ContainerApp = styled.div`
 	align-items: center;
 	background-color: #FB6B6B;
  `;
+
+ const Logo = styled.div`
+	margin-top: 20px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+	img {
+		width: 52px;
+		height: 60px;
+	}
+	h1 {
+		margin-left: 10px;
+		color: #FFF;
+		text-align: center;
+		font-family: Righteous;
+		font-size: 36px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+ `
