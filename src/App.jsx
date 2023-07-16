e663b34e651d01bb5751ade45d3b698bb9afc453
Theo <theo.piedade@gmail.com>
2023-07-16 09:47:29 -0300

    Layout init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import styled from 'styled-components';
+
+
+const cards = [
+	{ question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
+	{ question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+	{ question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
+	{ question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+	{ question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
+	{ question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
+	{ question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
+	{ question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <Body>
+   <ContainerApp>
+	Teste
+   </ContainerApp>
+   </Body>
   )
 }
 
 export default App
+
+const Body = styled.div`
+	background-color: grey;
+`
+
+const ContainerApp = styled.div`
+	width: 375px;
+	height: 667px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: #FB6B6B;
+ `;
